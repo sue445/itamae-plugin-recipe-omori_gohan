3e43694c17d97181727b92b1e8676b4d677a8c86
sue445 <sue445@sue445.net>
2016-01-22 13:03:51 +0900

    bundle gem itamae-plugin-recipe-omori_gohan --mit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in itamae-plugin-recipe-omori_gohan.gemspec
+gemspec
---
+++ lib/itamae/plugin/recipe/omori_gohan.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/omori_gohan/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module OmoriGohan
+        # Your code goes here...
+      end
+    end
+  end
+end
