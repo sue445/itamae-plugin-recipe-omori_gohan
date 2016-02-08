62a96779d353dbb76e32c1449343538ee8758325
sue445 <sue445@sue445.net>
2016-02-08 20:47:30 +0900

    Use src_dir instead of /tmp

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ include_recipe "omori_gohan"
 download_and_install_deb "vagrant" do
   url     "https://releases.hashicorp.com/vagrant/#{node[:vagrant][:version]}/vagrant_#{node[:vagrant][:version]}_x86_64.deb"
   version node[:vagrant][:version]
+  src_dir "/usr/local/src"
 end
 ```
 
--- lib/itamae/plugin/recipe/omori_gohan/definitions/download_and_install_deb.rb
+++ lib/itamae/plugin/recipe/omori_gohan/definitions/download_and_install_deb.rb
@@ -1,9 +1,10 @@
 # Download and install deb file
-define :download_and_install_deb, version: nil, url: nil do
+define :download_and_install_deb, version: nil, url: nil, src_dir: "/usr/local/src" do
   basename = File.basename(params[:url])
 
   name     = params[:name]
   version  = params[:version]
+  src_dir  = params[:src_dir]
 
   [
     "wget #{params[:url]} -O #{basename}",
@@ -12,7 +13,7 @@ define :download_and_install_deb, version: nil, url: nil do
     execute command do
       not_if "dpkg -l | grep #{name} | grep #{version}"
 
-      cwd "/tmp"
+      cwd src_dir
     end
   end
 end
