6e2e0195f7c9d1d320c8759cef31b2b958f6b4b2
sue445 <sue445@sue445.net>
2018-04-18 01:09:48 +0900

    Install wget

--- recipes/debian.rb
+++ recipes/debian.rb
@@ -1,3 +1,5 @@
+execute "apt-get update"
+
 download_and_install_deb "vagrant" do
   url     "https://releases.hashicorp.com/vagrant/#{node[:vagrant][:version]}/vagrant_#{node[:vagrant][:version]}_x86_64.deb"
   version node[:vagrant][:version]
